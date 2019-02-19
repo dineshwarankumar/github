5fdbe13cb5f630438904fd3f502140a1a182a2d7
dineshwarankumar <47665283+dineshwarankumar@users.noreply.github.com>
2019-02-19 22:09:07 +0530

    Create expfst.java

---
+++ expfst.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class expfst
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a>0)
+{
+System.out.print("Postive");
+}
+else if(a<0)
+{
+System.out.print("Negative");
+}
+else
+System.out.print("Zero");
+}
+}
