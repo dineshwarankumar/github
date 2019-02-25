5de66ba0b1bc85222760a0db80c7555254001e0b
dineshwarankumar <47665283+dineshwarankumar@users.noreply.github.com>
2019-02-25 11:32:19 +0530

    Create g6.java

---
+++ g6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class g6
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if(a%4==0)
+System.out.println("yes");
+else
+System.out.println("no");
+
+    }
+}
