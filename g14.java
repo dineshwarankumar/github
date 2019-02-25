f9aa8dc77b95176b669d67b0bd266bb941fc95b9
dineshwarankumar <47665283+dineshwarankumar@users.noreply.github.com>
2019-02-25 14:39:46 +0530

    Create g14.java

---
+++ g14.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class g14
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int m=s.nextInt();
+        int n=s.nextInt();
+        for(int i=m;i<n;i++)
+        {
+            if((i%2)!=0)
+            {
+            System.out.print(i+" ");
+            }
+        }
+        }
+    }
