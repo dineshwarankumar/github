93b151fe7beabed50436dc12264d6a118a7e0646
dineshwarankumar <47665283+dineshwarankumar@users.noreply.github.com>
2019-02-25 11:42:33 +0530

    Create g8.java

---
+++ g8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class g8
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int sum=0;
+        int a=s.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            sum=sum+i;
+}
+System.out.print(sum);
+    }
+}
