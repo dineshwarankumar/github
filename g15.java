147c9b84a070a9b6acd5a4879625b0ee89760b03
dineshwarankumar <47665283+dineshwarankumar@users.noreply.github.com>
2019-02-25 14:46:07 +0530

    Create g15.java

---
+++ g15.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class g15
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int m=s.nextInt();
+        int n=s.nextInt();
+        for(int i=m;i<n;i++)
+        {
+            if(i==1)
+            {
+            }
+            else if((i%2)==0)
+            {
+            System.out.print(i+" ");
+            }
+        }
+        }
+    }
