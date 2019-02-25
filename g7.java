524e1abf0f29f23eab4f6f1516a185236e89c65c
dineshwarankumar <47665283+dineshwarankumar@users.noreply.github.com>
2019-02-25 11:38:47 +0530

    Create g7.java

---
+++ g7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class g7
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        for(int i=0;i<a;i++)
+        {
+System.out.println("Hello");
+}
+    }
+}
