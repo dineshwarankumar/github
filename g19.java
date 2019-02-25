ab25a0bf50650045ff9bc913fc6cfd934622119f
dineshwarankumar <47665283+dineshwarankumar@users.noreply.github.com>
2019-02-25 15:55:59 +0530

    Create g19.java

---
+++ g19.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class g19
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int m=s.nextInt();
+        int tmp=1;
+        for(int i=1;i<=m;i++)
+        {
+            tmp=tmp*i;
+        }
+        System.out.print(tmp);
+        }
+    }
+
+
+
